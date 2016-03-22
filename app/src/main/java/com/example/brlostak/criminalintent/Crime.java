3ade6e7e7067c0016b7d4cb6ab68bc0b15a06b7b
rschildge <rschildge@gmail.com>
2016-03-22 13:13:39 +0100

    3/22/2016

--- app/src/main/java/com/example/brlostak/criminalintent/Crime.java
+++ app/src/main/java/com/example/brlostak/criminalintent/Crime.java
@@ -1,5 +1,8 @@
 package com.example.brlostak.criminalintent;
 
+import android.provider.ContactsContract;
+
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -8,10 +11,13 @@ import java.util.UUID;
 public class Crime {
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
 
     public Crime() {
         //UUID creates a unique identifier number?
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
 
     public String getTitle() {
@@ -25,4 +31,21 @@ public class Crime {
     public UUID getId() {
         return mId;
     }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
 }
